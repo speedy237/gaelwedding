ac3fe20c441d2761aa7ec899b5e85a9e8e58b4c7
speedy237 <joryiyueme@gmail.com>
2025-08-28 21:10:49 +0200

    update app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 const App: React.FC = () => (
-  <Router>
+  <Router basename="/gaelwedding/">
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow">
